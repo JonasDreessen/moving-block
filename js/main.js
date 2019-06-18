2e3f7aa2efa9d4ab4ae808a26400651f9e40cafa
JonasDreessen <dreessenjonas95@gmail.com>
2019-06-18 11:53:57 +0200

    add animation and a counter

--- js/main.js
+++ js/main.js
@@ -1,68 +1,86 @@
 const eatingBlock = document.getElementById('moving-block');
 const foodBlock = document.getElementById("food-block");
-var positionEatingBlock = 0;
+var positionEatingBlockX = 0;
+var positionEatingBlockY = 0;
 foodBlock.style.left = "50%";
 foodBlock.style.top = "50%";
-
+var scoopies = 0
+var aantalScoopies = document.getElementById("aantal-scoopies");
 var foodBlockPosition = foodBlock.getBoundingClientRect();
 
-
-
-/* function currentPositionMovingBlock() {
-    let eatingElement = document.getElementById('moving-block').getBoundingClientRect();
-    let eatTop = eatingElement.top;
-    let eatLeft = eatingElement.left;
-    let eatRight = eatingElement.right;
-    let eatBottom = eatingElement.bottom;
-    console.log(eatTop,eatLeft,eatRight,eatBottom);
-}
-
-function currentPositionFoodBlock(){
-    let foodElement = document.getElementById('food-block').getBoundingClientRect();
-    let foodTop = foodElement.top;
-    let foodLeft = foodElement.left;
-    let foodRight = foodElement.right;
-    let foodBottom = foodElement.bottom;
-    console.log(foodTop,foodLeft,foodRight,foodBottom);
-} */
-
 function eatMe(){
-let eatingElement = document.getElementById('moving-block').getBoundingClientRect();
-    let eatTop = eatingElement.top;
-    let eatLeft = eatingElement.left;
-    let eatRight = eatingElement.right;
-    let eatBottom = eatingElement.bottom;
-    console.log(eatTop,eatLeft,eatRight,eatBottom);
+    // get position of the moving-block
+var eatingElement = document.getElementById('moving-block').getBoundingClientRect();
+    var eatTop = eatingElement.top;
+    var eatLeft = eatingElement.left;
+    var eatRight = eatingElement.right;
+    var eatBottom = eatingElement.bottom;
+    // get position of the food-block
+    var foodElement = document.getElementById('food-block').getBoundingClientRect();
+    var foodTop = foodElement.top;
+    var foodLeft = foodElement.left;
+    var foodRight = foodElement.right;
+    var foodBottom = foodElement.bottom;
+    // Generate random number 
+    var RandomNumberLeft = Math.floor(Math.random() * 100);
+    var RandomNumberRight = Math.floor(Math.random() * 100);
+    var RandomNumberTop = Math.floor(Math.random() * 100);
+    var RandomNumberBottom = Math.floor(Math.random() * 100);
 
-    let foodElement = document.getElementById('food-block').getBoundingClientRect();
-    let foodTop = foodElement.top;
-    let foodLeft = foodElement.left;
-    let foodRight = foodElement.right;
-    let foodBottom = foodElement.bottom;
-    console.log(foodTop,foodLeft,foodRight,foodBottom);
+    if(eatTop <= foodTop && eatLeft <= foodLeft && eatRight >= foodRight && eatBottom >= foodBottom  ){
 
+
+        foodBlock.style.left = RandomNumberLeft + "%";
+        foodBlock.style.right = RandomNumberRight + "%";
+        foodBlock.style.top = RandomNumberTop + "%";
+        foodBlock.style.bottom = RandomNumberBottom + "%";
+        scoopies ++; 
+        aantalScoopies.innerHTML = scoopies;
+        console.log(scoopies);
+    }
 }
 
 function direction(e) {
-    if (e.keyCode == 39) {
-        positionEatingBlock += 2;
-        eatingBlock.style.left = positionEatingBlock + '%';
-        currentPositionMovingBlock();
-        currentPositionFoodBlock();
-    }
-    if (e.keyCode == 37) {
-        positionEatingBlock -= 2;
-        eatingBlock.style.left = positionEatingBlock + '%';
-
-    }
-    if (e.keyCode == 38) {
-        positionEatingBlock -= 2;
-        eatingBlock.style.top = positionEatingBlock + '%';
-    }
-    if (e.keyCode == 40) {
-        positionEatingBlock += 2;
-        eatingBlock.style.top = positionEatingBlock + '%';
+    // if right arrow is pressed
+    if (e.keyCode === 39){
+        goRight();
+    // if left arrow is pressed
+    } else if (e.keyCode === 37){
+        goLeft();
+    // if down arrow is pressed
+    } else if (e.keyCode === 40){
+        goDown();
+    // if up arrow is pressed
+    } else if (e.keyCode === 38){
+        goUp();
     }
 }
 
+// Makes the eatingBlock go right
+function goRight(){
+    positionEatingBlockX += 5;
+    eatingBlock.style.left = positionEatingBlockX + "%";
+    eatMe();
+}
+
+// Makes the eatingBlock go left
+function goLeft(){
+    positionEatingBlockX -= 5;
+    eatingBlock.style.left = positionEatingBlockX + "%";
+    eatMe();
+}
+
+// Makes the eatingBlock go down
+function goDown(){
+    positionEatingBlockY += 5;
+    eatingBlock.style.top = positionEatingBlockY + "%";
+    eatMe();
+}
+
+// Makes the eatingBlock go up
+function goUp(){
+    positionEatingBlockY -= 5;
+    eatingBlock.style.top = positionEatingBlockY + "%";
+    eatMe();
+}
 document.onkeydown = direction;
