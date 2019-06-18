a04e44243ecd6b8154e75d04be7b7558807c1645
JonasDreessen <dreessenjonas95@gmail.com>
2019-06-18 10:04:36 +0200

    get the position of the moving-block and the food-block

--- js/main.js
+++ js/main.js
@@ -1,24 +1,67 @@
-const murphy = document.getElementById('moving-block');
+const eatingBlock = document.getElementById('moving-block');
+const foodBlock = document.getElementById("food-block");
+var positionEatingBlock = 0;
+foodBlock.style.left = "50%";
+foodBlock.style.top = "50%";
 
-var position = 0;
+var foodBlockPosition = foodBlock.getBoundingClientRect();
+
+
+
+/* function currentPositionMovingBlock() {
+    let eatingElement = document.getElementById('moving-block').getBoundingClientRect();
+    let eatTop = eatingElement.top;
+    let eatLeft = eatingElement.left;
+    let eatRight = eatingElement.right;
+    let eatBottom = eatingElement.bottom;
+    console.log(eatTop,eatLeft,eatRight,eatBottom);
+}
+
+function currentPositionFoodBlock(){
+    let foodElement = document.getElementById('food-block').getBoundingClientRect();
+    let foodTop = foodElement.top;
+    let foodLeft = foodElement.left;
+    let foodRight = foodElement.right;
+    let foodBottom = foodElement.bottom;
+    console.log(foodTop,foodLeft,foodRight,foodBottom);
+} */
+
+function eatMe(){
+let eatingElement = document.getElementById('moving-block').getBoundingClientRect();
+    let eatTop = eatingElement.top;
+    let eatLeft = eatingElement.left;
+    let eatRight = eatingElement.right;
+    let eatBottom = eatingElement.bottom;
+    console.log(eatTop,eatLeft,eatRight,eatBottom);
+
+    let foodElement = document.getElementById('food-block').getBoundingClientRect();
+    let foodTop = foodElement.top;
+    let foodLeft = foodElement.left;
+    let foodRight = foodElement.right;
+    let foodBottom = foodElement.bottom;
+    console.log(foodTop,foodLeft,foodRight,foodBottom);
+
+}
 
 function direction(e) {
     if (e.keyCode == 39) {
-        position += 2;
-        murphy.style.left = position + '%';
+        positionEatingBlock += 2;
+        eatingBlock.style.left = positionEatingBlock + '%';
+        currentPositionMovingBlock();
+        currentPositionFoodBlock();
     }
     if (e.keyCode == 37) {
-        position -= 2;
-        murphy.style.left = position + '%';
+        positionEatingBlock -= 2;
+        eatingBlock.style.left = positionEatingBlock + '%';
 
     }
     if (e.keyCode == 38) {
-        position -= 2;
-        murphy.style.top = position + '%';
+        positionEatingBlock -= 2;
+        eatingBlock.style.top = positionEatingBlock + '%';
     }
     if (e.keyCode == 40) {
-        position += 2;
-        murphy.style.top = position + '%';
+        positionEatingBlock += 2;
+        eatingBlock.style.top = positionEatingBlock + '%';
     }
 }
 
